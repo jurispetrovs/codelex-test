71ead181ddd63ab55437dd3fd4642a4d83316cbc
Juris Petrovs <jura-petrov@inbox.lv>
2020-09-21 20:21:03 +0300

    Task 5 (namespaces) Bug Fix

---
+++ app/SpicesCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class SpicesCollection
+{
+    private array $spices = [];
+    public function add(Spice $spice): void
+    {
+        $this->spices[] = $spice;
+    }
+    public function all(): array
+    {
+        return $this->spices;
+    }
+}
--- task-5.php
+++ task-5.php
@@ -1,14 +1,20 @@
 <?php
 
-require_once 'app/Spices.php';
+require_once 'app/Spice.php';
+require_once 'app/SpicesCollection.php';
 require_once 'app/Spices/Salt.php';
 require_once 'app/Spices/Pepper.php';
+
+use App\SpicesCollection;
+use App\Spice;
 use App\Spices\Pepper;
 use App\Spices\Salt;
 
-$spices[] = new Pepper('Caribbean Red Habanero');
-$spices[] = new Salt('Sea salt');
+$spices = new SpicesCollection();
+$spices->add(new Pepper('Caribbean Red Habanero'));
+$spices->add(new Salt('Sea salt'));
 
-foreach ($spices as $spice) {
+foreach ($spices->all() as $spice) {
+    /** @var Spice $spice */
     echo $spice->getName() . ' ' . $spice->getWeight() . ' kg' . PHP_EOL;
 }
